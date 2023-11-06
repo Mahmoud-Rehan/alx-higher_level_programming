a9fc63e7eecd3d167f353cbadfac533bd1b2e3da
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-11-06 10:35:45 -0800

    Python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pytho3
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{}".format(0))
+    else:
+        n = 0
+        for i in range(1, len(argv)):
+            n += int(argv[i])
+        print("{}".format(n))
