5f615a97de766aecab8bac9e28d17834527c3ec5
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-11-10 10:17:46 -0800

    Python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    new = a_dictionary.copy()
     max_num = 0
     key = None
-    for k, v in new.item():
+    for k, v in a_dictionary.item():
         if v > max_num:
             max_num = v
             key = k
