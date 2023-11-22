3a806ec8631e33ea956811b6cad2987a5fa656e2
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-11-22 11:51:00 -0800

    Python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class Square module"""
+
+
+class Square:
+    """Define Square Class with private attribute size"""
+    def __init__(self, size):
+        self.__size = size
