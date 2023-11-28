b8c137547f80a4159e888c7237df495994831b90
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-11-28 13:11:08 -0800

    Python

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" FUNCTION test_indentation MODULE """
+
+
+def text_indentation(text):
+    """ FUNCTION THAT PRINTS 2 LINES
+    AFTER ., ?, : CHARACTERS.
+
+    ARGS:
+        test: STRING.
+
+    RAISES:
+        TypeError: IF text IS NOT A STRING.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for s in ":?.":
+        text = (s + '\n\n').join([l.strip(" ")
+            for l in text.split(s)])
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
