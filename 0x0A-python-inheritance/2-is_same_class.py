33e6411bde53028aa67c9e75ad5582f224f1be1b
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-05 11:16:43 -0800

    Python

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" is_same_class Function Module """
+
+
+def is_same_class(obj, a_class):
+    """ Function to check if obj
+    is of type a_class """
+
+    return (a_class == type(obj))
