c46d651d72989be3cbc39001e787d30c6a498be5
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-05 10:47:09 -0800

    Pyhon

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList Class Module """
+
+
+class MyList(list):
+    """ MyList Class """
+    def print_sorted(self):
+        """ Prints a sorted list """
+        print_sorted(self)
