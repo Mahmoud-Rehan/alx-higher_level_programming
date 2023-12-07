109703284c9d0edba285ba105eb393b3cb8a022d
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-07 13:40:23 -0800

    Python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ write_file FUNCTION MODULE """
 
+
 def write_file(filename="", text=""):
     """ write_file function """
     with open(filename, "w", encoding="utf-8") as f:
