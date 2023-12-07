c12acb1293b706f1bf53fa99ec4171375ff849b8
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-07 13:04:28 -0800

    Python

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" read_file FUNCTION MODULE """
+
+
+def def read_file(filename=""):
+    """ read_file function """
+    with open(filename, "r", encoding="utf-8") f:
+        print(f.read())
