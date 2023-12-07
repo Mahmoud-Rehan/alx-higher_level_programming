f3507c31d096e34ef5c86cc74a08a36aa6b29ab2
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-07 13:49:41 -0800

    Python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ append_write FUNCTION MODULE """
 
+
 def append_write(filename="", text=""):
     """ append_write function """
     with open(filename, "a", encoding="utf-8") as f:
