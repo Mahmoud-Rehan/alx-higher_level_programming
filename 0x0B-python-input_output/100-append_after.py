322bfd3fe5675ea88036c3d2105c641f7b92f16f
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-09 03:01:16 -0800

    Python

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" append_after FUNCTION MODULE """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ append_after function """
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = []
+        while (1):
+            line = f.readline()
+            if line == "":
+                break
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
+    with open(filename, "r", encoding="utf-8") as f:
+        f.writelines(lines)
