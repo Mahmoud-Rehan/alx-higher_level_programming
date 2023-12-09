be20ddca8e7e8f71d6468c5639a9e359be235e78
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-09 01:49:12 -0800

    Python

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Student CLASS MODULE """
+
 
 class Student:
-    """ initialization function """
+    """ Student class """
     def __init__(self, first_name, last_name, age):
+        """ initialization function """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
