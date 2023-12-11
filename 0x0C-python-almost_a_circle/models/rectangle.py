eb7d835042f96ff19b53f6dd016090c5bb5146ba
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-11 12:29:54 -0800

    Python

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Rectangle CLASS MODULE """
-base = __import__("base.py").Base
+from model.base import Base
 
 
 class Rectangle(Base):
@@ -8,7 +8,47 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initialization function """
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Get width function """
+        return (self.__width)
+
+    @width.setter
+    """ Set width function """
+    def width(self, width):
         self.__width = width
+
+    @property
+    def height(self):
+        """ Get height functin """
+        return (self.__height)
+
+    @height.setter
+    def height(self, height):
+        """ Set height function """
         self.__height = height
+
+    @property
+    def x(self):
+        """ Get x function """
+        return (self.__x)
+
+    @setter.x
+    def x(self, x):
+        """ Set x function """
         self.__x = x
+
+    @property
+    def y(self):
+        """ Get y function """
+        return (self.__y)
+
+    @y.setter
+    def y(self, y):
+        """ Set y function """
         self.__y = y
