f226013a0b99fdf9d1670847edcd262361c8f652
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-11 14:14:02 -0800

    Python

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,8 @@ class Rectangle(Base):
         p = "\n" * self.y + \
             (" " * self.x + "#" * self.width + "\n") * self.height
         print(p, end="")
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
+                                                self.id, self.x, self.y,
+                                                self.width, self.height)
