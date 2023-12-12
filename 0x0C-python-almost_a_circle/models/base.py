cab9860354de3f389f92eab631df1f0f28af31b2
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-12 09:49:03 -0800

    Python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,11 @@ class Base:
             list_objs = [obj.to_dictionary() for obj in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ from_json_string function """
+        if json_string is None:
+            return ([])
+        else:
+            return (json.loads(json_string)
