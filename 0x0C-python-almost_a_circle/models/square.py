a2616a32fd2b18ce89b91c1fdbb9646ccf8f464c
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-12-12 05:21:29 -0800

    Python

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """ Square class """
     def __init__(self, size, x=0, y=0, id=None):
         """ Initialization function """
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str function """
