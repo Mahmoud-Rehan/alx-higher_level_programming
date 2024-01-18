9dacab974145a9d2b1de9ab824b33348f4ce71cf
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-18 14:29:35 -0800

    Python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """ Add 2 values """
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} + {}".format(a, b, add(a, b)))
+    print(f"{a} + {b} = {add(a, b)}")
