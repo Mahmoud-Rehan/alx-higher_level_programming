65327eec1d3f99ee7c45d38710d4cf02b23cb5b4
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-18 15:52:19 -0800

    Python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
-        prit("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
