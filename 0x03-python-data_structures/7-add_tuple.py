a2b559e5cc738da44eef504415182cec3065ddbd
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-19 14:54:23 -0800

    Python

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x1 = tuple1[0] if len(tuple1) > 0 else 0
+    x2 = tuple1[1] if len(tuple1) > 1 else 0
+    y1 = tuple2[0] if len(tuple2) > 0 else 0
+    y1 = tuple2[1] if len(tuple2) > 1 else 0
+    return (x1 + y1, x2 + y2)
