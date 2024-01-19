870ad10e1eef89771a6d70288ec0c9cc0666a34a
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-19 14:49:00 -0800

    Python

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return (None)
+    for row in matrix:
+        if len(row) == 0:
+            print()
+        for n in range(len(row)):
+            print("{:d}".format(row[n]), end="\n"
+                   if n is len(row) - 1 else " ")
