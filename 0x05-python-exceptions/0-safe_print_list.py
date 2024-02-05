8091ef130329046b630d19e95e23669dd950e90c
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-05 21:28:29 +0000

    Pytho

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def safe_print_list(my_list=[], x=0):
+	n = 0
+
+	try:
+		while n < x:
+			print(my_list[n], end="")
+			n += 1
+	except IndexError:
+		pass
+	print()
+	return (n)
