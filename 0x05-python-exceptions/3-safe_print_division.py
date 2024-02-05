90b4bc819fa1abbcb044305b6e997acad790a4ef
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-05 14:52:27 -0800

    Python

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    n = 0
+    try:
+        n = a / b
+    except ZeroDivisionError:
+        n = None
+    finally:
+        print("Inside result: {}".format(n))
+        return n
