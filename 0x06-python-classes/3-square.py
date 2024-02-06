d9919104b9b38f18efe4029378ae2633f6d5c36e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-06 13:02:27 -0800

    Python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """ Area of the Square method """
-        return (self.__size * self.__size)
+        return (self.__size ** 2)
