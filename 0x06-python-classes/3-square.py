7142ef3ceb7bbf72c7048edf49033f10b84f3b2b
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-06 13:07:57 -0800

    Python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,16 @@ class Square:
     """ Square Class """
 
     def __init__(self, size):
-        """ Initialization method """
+        """ Initialization method 
+
+        Args:
+            size: Length of the square size.
+
+        Raises:
+            TypeError: Size is not int
+            ValueError: Size is less than 0
+
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,5 +23,9 @@ class Square:
         self.__size = size
 
     def area(self):
-        """ Area of the Square method """
-        return (self.__size ** 2)
+        """ Area of the Square method 
+
+        Returns: 
+            The square of size.
+        """
+        return (self.__size ** self.__size)
