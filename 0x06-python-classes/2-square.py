52693dffc792079941d9caa1444b277b2360dd8e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-06 12:52:14 -0800

    Python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Square class module """
 
+
 class Square:
     """ Square Class """
     def __init__(self, size=0):
@@ -9,4 +10,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
