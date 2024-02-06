4644272a8729a4bc8f835f432fa09fe496455d79
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-06 15:05:21 -0800

    Python

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Square Class Module """
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initialization function """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """ Retrieve the size function  """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Update the size function """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Retrieve the position function """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """ Update the position function """
+        if (type(value) is not tuple or
+                len(value) != 2 or
+                not all(type(n) is int for n in value) or
+                not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+            self.__positon = value
+
+    def area(self):
+        """ Get the area of the Square function """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ Print the Square function """
+        if self.__size == 0:
+            print()
+            return
+
+        [print() for n in range(0, self.__position[1])]
+        for j in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
