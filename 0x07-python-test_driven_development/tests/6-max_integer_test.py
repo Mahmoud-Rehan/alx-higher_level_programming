f2f8b75ea159a83cf769210beb8e9889da50b89a
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-14 07:14:02 -0800

    Tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,6 +11,22 @@ class TestMaxInteger(unittest.TestCase):
         """Unittest for max_integer([..])"""
         self.assertEqual(max_integer(), None)
 
+    def test_empty_list(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_one(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([98]), 98)
+
+    def test_identical(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([7, 7, 7, 7]), 7)
+
+    def test_max_start(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([5, 4, 3, 2]), 5)
+
     def test_ordered(self):
         """Unittest for max_integer([..])"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
@@ -50,6 +66,31 @@ class TestMaxInteger(unittest.TestCase):
                     -4104, 5772, 5121, -2186, -4870, -4116, 6443, -9381,
                     -9388, 8552, 3582, 3500, 7924, 211, -2976, 6346, -5405,
                     899, -3432, -2550, -3353, 6944, 9623]), 9823)
+
+    def test_negatives(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(
+            max_integer(
+                [-6105619, -854849, -562553, -3088955, -6711290, -4817844,
+                    -1907189, -8110534, -6601769, -5837524, -4726702,
+                    -8433749, -7251403, -5117635, -2979207, -1335257,
+                    -6867266, -9073637, -6224732, -1080801, -1080228,
+                    -6801278, -8351954, -1736432, -746131, -4376995,
+                    -967891, -4663691, -71562, -7153670, -8038202,
+                    -7893047, -9350883, -1132134, -3675971, -8495354,
+                    -9158229, -9310087, -6319598, -8961209, -4906000,
+                    -386471, -639929, -2676965, -6881679, -6258057,
+                    -5490677, -1107298, -4199148, -5933601, -9917695,
+                    -7759849, -7045734, -4885806, -9485075, -5119579,
+                    -4147063, -7622811, -4671971, -5439539, -840414,
+                    -3671742, -4400074, -3549343, -9146070, -6071672,
+                    -7213213, -1307446, -3936098, -2415520, -9162654,
+                    -6129976, -5791439, -3481890, -7828832, -6954726,
+                    -5272933, -4952516, -6115545, -8333464, -7271456,
+                    -4097027, -4342575, -8400559, -8235052, -4373818,
+                    -8054214, -8565596, -639225, -2292452, -4269529,
+                    -7202853, -6891036, -4379807, -7955196, -1536591,
+                    -2839083, -2586661, -9941097, -3136620]), -71562)
 
     def test_ints_and_floats(self):
         """Unittest for max_integer([..])"""
@@ -150,9 +191,23 @@ class TestMaxInteger(unittest.TestCase):
                     0.20607476376994402, 0.9497689034126077,
                     2.1498649449691807]), 29.496355326217376)
 
+    def test_numeric_string(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer("192834754"), "9")
+
     def test_string(self):
         """Unittest for max_integer([..])"""
         self.assertEqual(max_integer("Holberton"), "t")
+
+    def test_lists(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(max_integer([[], [2], [4], [2, 9]]), [4])
+
+    def test_str_list(self):
+        """Unittest for max_integer([..])"""
+        self.assertEqual(
+            max_integer([["foo"], ["boo"], ["abc"], ["sic"], ["ric"]]),
+            ["sic"])
 
     def test_inf(self):
         """Unittest for max_integer([..])"""
@@ -178,20 +233,6 @@ class TestMaxInteger(unittest.TestCase):
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def test_dict(self):
-        """Unittest for max_integer([..])"""
-        with self.assertRaises(TypeError):
-            max_integer([{20: 23, 14: 45}, {"a": "b"}])
-
-    def test_int(self):
-        """Unittest for max_integer([..])"""
-        with self.assertRaises(TypeError):
-            max_integer(98)
-
-    def test_float(self):
-        """Unittest for max_integer([..])"""
-        with self.assertRaises(TypeError):
-            max_integer(9.8)
 
 if __name__ == '__main__':
     unittest.main()
