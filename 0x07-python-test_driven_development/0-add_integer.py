c9265f7b75d2cde5ed3eb5da6a14714d219fe50e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-14 00:09:24 -0800

    Pyhon

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 def add_integer(a, b=98):
     """ Add two integers function """
-    if not isinstance(a, int) or not isinstance(a, float):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
