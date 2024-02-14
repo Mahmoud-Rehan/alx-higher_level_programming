de721dec62a612942917323f4846c1a928696ffe
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-14 07:26:56 -0800

    Python

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,6 +9,7 @@ def lazy_matrix_mul(m_a, m_b):
     """ Multiply the matrix function """
     return (matmul(m_a, m_b))
 
+
 if __name__ == "__main__":
     import doctest
-    doctest,testfile("tests/101-lazy_matrix_mul.txt")
+    doctest.testfile("tests/101-lazy_matrix_mul.txt")
