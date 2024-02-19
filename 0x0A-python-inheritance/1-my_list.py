4d90067f0fe42c94fe76ab9586fd7a6e46d2a593
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-19 13:01:22 -0800

    Python

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList Cass Module """
+
+
+class MyList:
+    """ MyList Class """
+    def print_sorted(self):
+        """ Printsorted list method """
+        print(sorted(self))
