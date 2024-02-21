1b938a4e487f1707bba81f948592f002a0359765
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-21 10:19:47 -0800

    Python

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Student Class Module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        """ Initialization method """
+        self.first_name = first_name
+        self.last-name = last_name
+        self.age = age
+
+    def def to_json(self):
+        """ Retrieve student's dict representation method """
+        return (self.__dict__)
