089812b790ff5b4d5cebc924cef1a9beb2b5540e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-20 16:25:25 -0800

    Python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" read_file Function Module """
+"""Module containing the function read_file"""
 
 
 def read_file(filename=""):
-    """ Reads data from a file function """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        text = f.read()
-        print(text, end="")
+    """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
