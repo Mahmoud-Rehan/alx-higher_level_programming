5a36e4514233d73d6f5155ac8977e89839810ad3
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-20 17:37:04 -0800

    Python

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" class_to_json Function Module """
+
+
+def class_to_json(obj):
+    """ Give dictionary description
+    with simple data structure Function """
+    return (obj.__dict__)
