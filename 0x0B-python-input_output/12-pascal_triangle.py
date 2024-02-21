09af0dc1cf5354ae54ca30e11de15fc1ebe06987
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-21 14:55:38 -0800

    Python

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,13 +6,12 @@ def pascal_triangle(n):
     """ Create pascal triangle method """
     if n <= 0:
         return ([])
-
     if n == 1:
         return ([[1]])
 
     triangle = [[1]]
 
     for k in range(n - 1):
-        triangle.append([i + n for i, n in zip(triangle[-1] + [0],
-                                               [0] + pascal[-1])])
+        triangle.append([x + n for x, n in zip(triangle[-1] + [0],
+                                             [0] + triangle[-1])])
     return (triangle)
