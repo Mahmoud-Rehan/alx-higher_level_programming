4408f68111465e66d7d6748a8c6ee6f208a0b87b
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-20 16:13:40 -0800

    Python

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" write_file Function Module """
+
+
+def write_file(filename="", text=""):
+    """ Write text to a file function """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
