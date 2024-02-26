823a639102fcbe035b9ee7ce2d31b89900e87a81
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-26 13:01:24 -0800

    Python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+""" Base Class Module """
 
 
 class Base:
+    """ Base Class """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Initialization method """
         if id is not None:
             self.id = id
         else:
