e16b749fc90d3b93aef2a2dbc670925105fbc576
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-28 11:06:04 -0800

    Python

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,18 @@ class Square(Rectangle):
         """ Class string representation """
         return ("[Square] ({id}) {x}/{y} - {size}"
                 .format(id=self.id, x=self.x, y=self.y, size=self.width))
+
+    @property
+    def size(self):
+        """ Retrieve the value of size method """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """ Update the size value method """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
