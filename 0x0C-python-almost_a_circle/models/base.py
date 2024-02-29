e75ed64f4f8cea7a6e55bbd07c96eeec4d19af8a
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-29 10:41:40 -0800

    Python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ Get the json string representation """
         if list_dictionaries is None or not list_dictionaries:
             return ("[]")
-
         return (json.dumps(list_dictionaries))
