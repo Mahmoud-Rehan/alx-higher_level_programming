598d3c0d017edc34414adb92e9dba1ca7c00759e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-29 10:57:15 -0800

    Python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base Class Module """
 import json
+import csv
 
 
 class Base:
@@ -21,3 +22,12 @@ class Base:
         if list_dictionaries is None or not list_dictionaries:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes json string representation to a file """
+        if list_objs is not None:
+            list_objs = [obj.to_dictionary() for obj in list_objs]
+        filename = ("{}.json".format(cls.__name__))
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
