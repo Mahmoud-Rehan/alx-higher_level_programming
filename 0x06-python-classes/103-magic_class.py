5ed3074d40344c3b236109bd0f7d1c2c8f4e4b59
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-03-13 15:12:32 -0700

    Python

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" MagicClass Class Module """
+import math
+
+
+class MagicClass:
+    """ MagicClass Class """
+    def __init__(self, radius=0):
+        """ Initialization Method """
+        self.__radius = 0
+        if not isinstance(radius, int) and not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """ Calculate the area method """
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """ Calculate the circumference method """
+        return (2 * self.pi * self.__radius)
