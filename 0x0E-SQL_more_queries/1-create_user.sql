8e1d562df76593ca684765e87aa050310924a390
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-03-27 17:46:51 -0700

    MYSQL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates the MySQL server user
-CREATE USER IF NOT EXIST "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
-GRANT SELECT ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
+CREATE USER IF NOT EXIST "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_2"@"localhost";
