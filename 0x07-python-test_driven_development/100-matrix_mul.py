d705a727951e1a3ca345339ad226cf1ba4e38335
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-04-13 16:04:00 -0700

    Python

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -31,7 +31,7 @@ def matrix_mul(m_a, m_b):
     for b_row in m_b:
         for b_element in b_row:
             if not isinstance(b_element, int) and not isinstance(b_element, float):
-                raise TypeError("m_a should contain only integers or floats")
+                raise TypeError("m_b should contain only integers or floats")
 
     if all(len(a_row) != len(m_a[0]) for a_row in m_a):
         raise TypeError("each row of m_a must be of the same size")
