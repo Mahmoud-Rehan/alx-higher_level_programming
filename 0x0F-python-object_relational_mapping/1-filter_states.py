4e3d55b85dfbbe3f120d0e001e695b1304240fc6
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-25 17:40:13 -0700

    ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script that lists all
+    states starting with N """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                               host="localhost", port=3306, db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
