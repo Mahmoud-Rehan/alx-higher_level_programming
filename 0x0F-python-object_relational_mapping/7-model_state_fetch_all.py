2b201b0a90e8a4b0ab3c891f5ec84e39bd1be4cd
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-28 16:38:32 -0700

    SQLALCHEMY

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:\
+                            {argv[2]}@localhost:3306/{argv[3]}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
