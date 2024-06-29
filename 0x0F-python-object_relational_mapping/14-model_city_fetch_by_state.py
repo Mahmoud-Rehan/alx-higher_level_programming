240dda7e9c49b8f8f0eea450067211faabc73df6
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-28 19:44:35 -0700

    SQLALCHEMY

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that lists all State objects from the database hbtn_0e_6_usa """
+""" Script that lists all city objects from the database hbtn_0e_6_usa """
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
