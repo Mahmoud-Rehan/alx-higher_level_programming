781040a3ff28c5eb11c1d015cd4a236950779ffb
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-28 18:00:09 -0700

    SQLALCHEMY

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" Script that lists all State objects that
-    contain the letter a from
-    the database hbtn_0e_6_usa
+""" Script that lists all State objects that contain the letter a
 """
 
 from model_state import Base, State
