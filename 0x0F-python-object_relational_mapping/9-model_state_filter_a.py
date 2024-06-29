8bb7d64cb2b490b9d76d8ff23cf2a967d225bd5f
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-28 18:02:00 -0700

    SQLALCHEMY

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,14 +8,16 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
-engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
-                        @localhost:3306/{argv[3]}")
+if __name__ == "__main__":
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                            @localhost:3306/{argv[3]}")
 
-states = session.query(State).filter(State.name.like("%a%"))\
-        .order_by(State.id).all()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for state in states:
-    print(f"{state.id}: {state.name}")
+    states = session.query(State).filter(State.name.like("%a%"))\
+            .order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
