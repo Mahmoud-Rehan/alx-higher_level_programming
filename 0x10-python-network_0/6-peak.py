7dd50a1f4e1d48a9dfdebeb90147f7e200153959
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-07-04 18:49:15 -0700

    Python

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     """ find_peak Function """
-    if list_of_integers is None or list_of_integers == []:
+    if list_of_integers == []:
         return (None)
 
     length = len(list_of_integers)
