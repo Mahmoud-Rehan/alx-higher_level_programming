729edb4210d98781461cfb2c94eeff5de7f47f9f
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-07-05 20:00:33 -0700

    python

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,15 +2,13 @@
 """ Python script that list 10 commits
     (from the most recent to oldest)
     of the repository """
-import reqests
+import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
-
     request = requests.get(url)
-
     json = request.json()
 
     try:
