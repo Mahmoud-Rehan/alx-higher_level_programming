7146b9baae7a463f3cc81defc9fb3c354531029e
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-07-07 19:03:49 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,13 +2,14 @@
 """ City Class Module """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_state  import Base
+from relationship_state import Base
 
 
 class City(Base):
     """ City Class """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
