59bc63233b718c578021f8f0b59cca99ade8fa9b
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-07-07 19:48:52 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,7 +15,9 @@ if __name__== "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in states:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
             print(f"\t{city.id}: {city.name}")
