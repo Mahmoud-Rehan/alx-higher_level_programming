81578eb970b4fb7d7df73a4be005407381fe879c
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-07-10 14:53:33 -0700

    Python

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     with urlopen(request) as response:
         body = response.read()
         decoded = body.decode("utf-8")
-        print(f"Email: {decoded}")
+        print(f"{decoded}")
